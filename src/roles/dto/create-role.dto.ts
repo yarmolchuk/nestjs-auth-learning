d7625d145247a3020f78a3823f6afc59a43993db
Yarmolchuk Dmytro <yarmolchuk@gmail.com>
2026-01-17 22:46:46 +0200

    Added validation.

--- src/roles/dto/create-role.dto.ts
+++ src/roles/dto/create-role.dto.ts
@@ -1,4 +1,13 @@
+import { IsString, IsNotEmpty, Length } from "class-validator";
+
 export class CreateRoleDto {
+    @IsString({message: 'Value must be a string'})
+    @IsNotEmpty({message: 'Value is required'})
+    @Length(2, 50, {message: 'Value must be between 2 and 50 characters'})
     readonly value: string;
+
+    @IsString({message: 'Description must be a string'})
+    @IsNotEmpty({message: 'Description is required'})
+    @Length(5, 255, {message: 'Description must be between 5 and 255 characters'})
     readonly description: string;
 }
